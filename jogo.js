5702e6231e0e439c0559c640f77e3b2f55b98507
Vinicius-Fregonesi <viniciusfregone@gmail.com>
2020-07-30 16:15:17 -0300

    Inicial

---
+++ jogo.js
@@ -0,0 +1,96 @@
+console.log('[DevSoutinho] Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+const chao = {
+  spriteX: 0,
+  spriteY: 610,
+  largura: 224,
+  altura: 112, 
+  x: 0,
+  y: canvas.height - 50,
+  desenha(){
+    contexto.drawImage(
+    sprites,
+    chao.spriteX, chao.spriteY,
+    chao.largura, chao.altura, //Tamanho do recorte na sprite
+    chao.x, chao.y,
+    chao.largura, chao.altura,
+    );
+    contexto.drawImage(
+      sprites,
+      chao.spriteX, chao.spriteY,
+      chao.largura, chao.altura, //Tamanho do recorte na sprite
+      (chao.x + chao.largura), chao.y,
+      chao.largura, chao.altura,
+      );
+  }
+}
+
+const planoDeFundo = {
+  spriteX: 390,
+  spriteY: 0,
+  largura: 275,
+  altura: 204, 
+  x: 0,
+  y:canvas.height - 204,
+  desenha(){
+    contexto.fillStyle= '#70c5ce';
+    contexto.fillRect(0,0, canvas.width,canvas.height);
+
+    contexto.drawImage(
+    sprites,
+    planoDeFundo.spriteX, planoDeFundo.spriteY,
+    planoDeFundo.largura, planoDeFundo.altura, //Tamanho do recorte na sprite
+    planoDeFundo.x, planoDeFundo.y,
+    planoDeFundo.largura, planoDeFundo.altura,
+    );
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      planoDeFundo.largura, planoDeFundo.altura, //Tamanho do recorte na sprite
+      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+      planoDeFundo.largura, planoDeFundo.altura,
+      );
+  }
+}
+
+
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  largura: 33,
+  altura: 24, 
+  x: 10,
+  y:50,
+  velocidade:0,
+  gravidade: 0.25,
+  atualiza(){
+    flappyBird.velocidade+= flappyBird.gravidade;
+    flappyBird.y+= flappyBird.velocidade;
+  },
+  desenha(){
+    contexto.drawImage(
+    sprites,
+    flappyBird.spriteX, flappyBird.spriteY,
+    flappyBird.largura, flappyBird.altura, //Tamanho do recorte na sprite
+    flappyBird.x, flappyBird.y,
+    flappyBird.largura, flappyBird.altura,
+    );
+  }
+}
+
+function loop(){
+  flappyBird.atualiza();
+  planoDeFundo.desenha();
+  chao.desenha();
+  flappyBird.desenha();
+  
+  requestAnimationFrame(loop);
+}
+
+loop();
